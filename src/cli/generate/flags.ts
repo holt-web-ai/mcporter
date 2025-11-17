95072a4622f4036c19c2aa309e1e023e619f3896
Peter Steinberger <steipete@gmail.com>
2025-11-17 20:31:55 +0100

    refactor(config): modularize import helpers

--- src/cli/generate/flags.ts
+++ src/cli/generate/flags.ts
@@ -1,6 +1,11 @@
 import { splitCommandLine } from '../adhoc-server.js';
 import { expectValue } from '../flag-utils.js';
-import { extractHttpServerTarget, looksLikeHttpUrl, normalizeHttpUrlCandidate, splitHttpToolSelector } from '../http-utils.js';
+import {
+  extractHttpServerTarget,
+  looksLikeHttpUrl,
+  normalizeHttpUrlCandidate,
+  splitHttpToolSelector,
+} from '../http-utils.js';
 import { extractGeneratorFlags } from './flag-parser.js';
 import type { CommandInput } from './types.js';
 
